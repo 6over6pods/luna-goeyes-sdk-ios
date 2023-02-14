aa466b09e9e25607a2079f24f4afa69e670c922b
Eugene Gordenstein <eugene@6over6.com>
2023-02-14 17:23:06 +0200

    [release-5.38.1] - Manifest file update

--- Package.swift
+++ Package.swift
@@ -5,24 +5,39 @@ import PackageDescription
 
 let package = Package(
     name: "GoEyesSDK",
+    defaultLocalization: "en-US",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GoEyesSDK",
-            targets: ["GoEyesSDK"]),
+            targets: ["GoEyesSDKWrapper"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/mixpanel/mixpanel-swift",
+            "3.2.0" ..< "4.0.0"
+        ),
+        .package(
+            url: "https://github.com/kirualex/SwiftyGif.git",
+            "5.0.0" ..< "6.0.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
+            name: "GoEyesSDKWrapper",
+            dependencies: [.product(name: "Mixpanel", package: "mixpanel-swift"),
+                           .product(name: "SwiftyGif", package: "swiftygif"),
+                           .target(name: "GoEyesSDK")]
+        ),
+        .binaryTarget(
             name: "GoEyesSDK",
-            dependencies: []),
-        .testTarget(
-            name: "GoEyesSDKTests",
-            dependencies: ["GoEyesSDK"]),
+            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/5.38.1/GoEyesSDK.xcframework.zip",
+            checksum: "4ec9e0b10da55b3df946c4dcf2bdad239ac86ab5ec5f8ec23d83f693d10c125f"
+        )
     ]
 )
