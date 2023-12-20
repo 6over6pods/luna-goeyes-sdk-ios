ed29e947f162b2b8708986d673ad8d2ad3b736d9
Eugene Gordenstein <eugene@6over6.com>
2023-12-20 10:34:27 -0500

    [release-5.44.4] - Framework update

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GoEyesSDK",
-            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/5.44.3/GoEyesSDK.xcframework.zip",
-            checksum: "131f1b25555269b38681b39983dcd05ef2563036e6429ed7efbbfc593440232c"
+            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/5.44.4/GoEyesSDK.xcframework.zip",
+            checksum: "c7a6738eca63cf30dd53b107cd0d90e9b5a79a369ca13af0aea105db67579671"
         )
     ]
 )
