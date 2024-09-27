d6df704d28b70e52b488aa068b19741aaa8790b5
Eugene Gordenstein <eugene@6over6.com>
2024-09-27 14:53:30 -0400

    Dependency Update

--- Package.swift
+++ Package.swift
@@ -16,23 +16,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/apple/swift-collections.git",
-            .upToNextMinor(from: "1.1.0")
-        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GoEyesSDKWrapper",
-            dependencies: [.target(name: "GoEyesSDK"),
-                           .product(name: "Collections", package: "swift-collections")]
+            dependencies: [.target(name: "GoEyesSDK")]
         ),
         .binaryTarget(
             name: "GoEyesSDK",
-            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/5.45.1/GoEyesSDK.xcframework.zip",
-            checksum: "b815c5974127cc903421bc689baced58c279589658b1da0d6ffc321f512ef0de"
+            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/5.45.11-1778/GoEyesSDK.xcframework.zip",
+            checksum: "d98f296e2add7dcd4b414bef199afc65beb66e228cf21e782d47c9017ffde133"
         )
     ]
 )
