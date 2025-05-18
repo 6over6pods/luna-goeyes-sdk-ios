049053ad708ba90ba5614af90a56ebc744044848
deployer-6over6 <deployer-6over6@6over6.com>
2025-05-18 12:22:06 +0000

    deployer-6over6: opening branch with changes in Package.swift

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GoEyesSDK",
-            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/5.46.4-2164/GoEyesSDK.xcframework.zip",
-            checksum: "30843d4c150a12ad6052ec7cb018a90c75eded6cc063352e03030ceccdeb0dac"
+            url: "https://glasseson.jfrog.io/artifactory/luna-goeyes-sdk-ios-local/6.0.6-3325/GoEyesSDK.xcframework.zip",
+            checksum: "b84e46db0e1b9f8d717dd5dd40c2ef15c344615eb87abc7560e8f97b8b90cd50"
         )
     ]
 )
